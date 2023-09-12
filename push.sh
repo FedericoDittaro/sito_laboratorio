212af21832e6ed15374f7bbf57c20aa1577e4ce0
FedericoDittaro <fede.dittaro@gmail.com>
2023-09-12 11:25:08 +0200

    cambiata la sezione Abou us

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eo pipefail
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $current_branch != Federico ]; then
+    echo WARNING: $current_branch not Federico
+fi
+
+if [-z $1]; then
+    echo ERROR: manca il commit message
+    exit 1
+fi    
+
+commit_message=$1
+
+hugo
+git add .
+git commit -m "$commit_message"
+git subtree push --prefix public origin gh-pages
